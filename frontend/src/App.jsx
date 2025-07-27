9c0aed9195c7797222c374f34dd496036336b659
20060602-hub <20060602@mydbs.ie>
2025-07-28 00:56:08 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,26 +24,25 @@
 
 // export default App;
 import { Routes, Route } from 'react-router-dom';
-import Signup from './pages/SignUp';
-import Signin from './pages/SignIn';
+import Signup from './pages/SignUp';  // Import your SignUp component
+import Signin from './pages/SignIn';  // Import your SignIn component
 import Home from './pages/Home';
 import OtpVerify from './pages/OtpVerify';
 import FingerprintVerify from './pages/FingerprintVerify';
 import VerifiedUsers from './pages/VerifiedUsers';
-import ChatPage from './pages/ChatPage';  // Only one chat page
+import ChatPage from './pages/ChatPage';
 
 const App = () => {
   return (
     <Routes>
-      {/* Keep only one route for Signup */}
+      <Route path="/" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} /> {/* Ensure /signin route is defined here */}
       <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<Signup />} /> {/* Default route */}
-      <Route path="/Signin" element={<Signin />} />
       <Route path="/verify-otp" element={<OtpVerify />} />
       <Route path="/fingerprint" element={<FingerprintVerify />} />
       <Route path="/home" element={<Home />} />
       <Route path="/verified-users" element={<VerifiedUsers />} />
-      <Route path="/chat" element={<ChatPage />} /> {/* Only one chat page */}
+      <Route path="/chat" element={<ChatPage />} />
     </Routes>
   );
 };
