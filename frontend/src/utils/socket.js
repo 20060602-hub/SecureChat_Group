99660b02e949f1b79b4a28114b62eaf1d9f525da
20060602-hub <20060602@mydbs.ie>
2025-07-28 01:09:12 +0100

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 // src/utils/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // backend socket server
+const socket = io('https://securechat-group.onrender.com/'); // backend socket server
 
 //export default socket;
