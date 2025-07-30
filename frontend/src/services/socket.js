fac7cc084029ff295c7ac215197aa70ad8d1680b
20060602-hub <20060602@mydbs.ie>
2025-07-30 14:41:12 +0100

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,4 +1,11 @@
 // services/socket.js
-import { io } from "socket.io-client";
-const socket = io(import.meta.env.VITE_BACKEND_URL, { autoConnect: false });
+import { io } from 'socket.io-client';
+
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ['websocket'],
+  secure: true,
+  withCredentials: true,
+  autoConnect: false, // ✅ you manually connect in components
+});
+
 export default socket;
