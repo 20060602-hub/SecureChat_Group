5b01338698946c0fef73b1eebbb7ec0a197a4e28
20060602-hub <20060602@mydbs.ie>
2025-07-30 18:40:52 +0100

    Update AESKey.js

--- backend/models/AESKey.js
+++ backend/models/AESKey.js
@@ -16,6 +16,7 @@
 // export default mongoose.model('AESKey', AESKeySchema);
 // models/AESKey.js
 import mongoose from 'mongoose';
+
 const AESKeySchema = new mongoose.Schema({
   users: {
     type: [String], // Sorted [email1, email2]
@@ -27,4 +28,7 @@ const AESKeySchema = new mongoose.Schema({
     required: true
   }
 });
+
+export default mongoose.model('AESKey', AESKeySchema);
+
 
