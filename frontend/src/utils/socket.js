f51ae195d5187aadea97a8f653b6c5379afd2a19
20060602-hub <20060602@mydbs.ie>
2025-07-30 14:56:12 +0100

    Create socket.js

---
+++ frontend/src/utils/socket.js
@@ -0,0 +1,11 @@
+
+import { io } from 'socket.io-client';
+
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ['websocket'],
+  secure: true,
+  withCredentials: true,
+  autoConnect: false, // ✅ you manually connect in components
+});
+
+export default socket;
